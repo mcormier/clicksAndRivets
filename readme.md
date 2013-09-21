278042da604be84bacfdc12674c1aebb269e2e72
Matthieu Cormier <mcormier@cs.dal.ca>
2013-09-21 13:16:21 -0300

    Every Minute on the Minute structure.

--- readme.md
+++ readme.md
@@ -1,4 +1,9 @@
-http://www.speedbagforum.com/timer.html
-http://www.beach-fitness.com/tabata/
-http://www.elated.com/articles/creating-a-javascript-clock/
+I was not impressed with the free javascript workout timers that I found online so I created one of my own.
+
+
+** References **
+
+- http://www.speedbagforum.com/timer.html
+- http://www.beach-fitness.com/tabata/
+- http://www.elated.com/articles/creating-a-javascript-clock/
 
--- src/js/PPEMOTMClock.js
+++ src/js/PPEMOTMClock.js
@@ -1,4 +1,62 @@
 // Stub for Every minute on the minute clock
-function PPEmotmClock () {
+function PPEmotmClock (displayElementID, startButtonID, stopButtonID ) {
+  this.displayElementID = displayElementID;
+  this.startButtonID = startButtonID;
+  this.stopButtonID = stopButtonID;
 
+  // TODO -- create and use a countdown clock
+  this.workoutClock = new PPClock();
+  
+  this.sound = new Audio('sound/short1.wav');
+
+  var that = this;
+  var runBinder = function () { that.display(); }
+  this.workoutClock.runAction = runBinder;
+
+  var initBinder = function() { that.init(); }
+  PPUtils.bind("load", window, initBinder );
+}
+
+
+PPEmotmClock.prototype.init = function () {
+  this.displayElement = $(this.displayElementID);
+
+  var that = this;
+  var startBinder = function() {  that.start(); }
+  var stopBinder = function() {  that.stop(); }
+  PPUtils.bind("click", $(this.stopButtonID), stopBinder );
+  PPUtils.bind("click", $(this.startButtonID), startBinder );
+}
+
+PPEmotmClock.prototype.start = function () {
+   this.workoutClock.start();
+}
+
+PPEmotmClock.prototype.stop = function () {
+  this.workoutClock.stop();
+}
+
+PPEmotmClock.prototype.format = function (secondsValue) {
+  var minutes = Math.floor(secondsValue / 60);
+  var seconds = secondsValue % 60;
+  
+  if ( seconds < 10 && minutes > 0) {
+    seconds = "0" + seconds;
+  }
+  
+  return minutes + ":" + seconds;
+}
+
+
+PPEmotmClock.prototype.display = function () {
+  var currValue = this.workoutClock.currentValue();
+  var currStringValue = this.format(currValue);
+
+  if ( currValue % 60 == 0 ) {
+    this.sound.play();
+  }
+
+  this.displayElement.innerHTML = currStringValue;
+  
+  
 }
