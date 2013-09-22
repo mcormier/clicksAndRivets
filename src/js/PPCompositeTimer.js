9e4ffa7f532d300a543f9b6908751733fee97e76
Matthieu Cormier <mcormier@cs.dal.ca>
2013-09-22 14:03:58 -0300

    implement a composite timer

--- src/js/PPCompositeTimer.js
+++ src/js/PPCompositeTimer.js
@@ -3,7 +3,34 @@
 // Usually one is short countdown timer,
 // i.e. 10 second countdown before starting a 20 minute
 //      workout
-function PPCompositeTimer(startTimer, mainTimer) {
+function PPCompositeTimer(idPrefix, startTimer, mainTimer) {
   this.startTimer = startTimer;
+  this.startTimer.setDelegate(this);
   this.mainTimer = mainTimer;
+  this.currentTimer = this.startTimer;
+
+  var timerBinder = new PPTimerBinder(idPrefix);
+  timerBinder.setDelegate(this);
 }
+
+PPCompositeTimer.prototype.setDisplayElement = function (element) {
+  this.startTimer.displayElement = element;
+  this.mainTimer.displayElement = element;
+};
+
+
+PPCompositeTimer.prototype.start = function () {
+  this.currentTimer.start();
+};
+
+
+PPCompositeTimer.prototype.stop = function () {
+  this.currentTimer.stop();
+};
+
+PPCompositeTimer.prototype.done = function (timer) {
+  PPUtils.log("Done");
+  if ( timer == this.startTimer ) {
+    this.mainTimer.start();
+  }
+};
